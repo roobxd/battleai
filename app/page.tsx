42641288b1e26a21f4f4cfc7eb16acd3ceaf634a
Robin Hummeling <robin.hummeling@hva.nl>
2024-09-15 19:38:24 +0200

    Added 'cartoon' styling to text input

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex bg-blue-500 min-h-screen min-w-screen">
+		<div className="flex bg-gradient-to-b from-indigo-800 to-indigo-600 min-h-screen min-w-screen">
 			<div className="flex flex-col justify-center items-center w-full">
 				<span className="font-bold text-7xl">Project Battle AI</span>
 				<div className="mt-12 space-y-4">
-					<span className="font-bold text-3xl">What's your name?</span>
-					<Input type="username" id="username" placeholder="Username" className="placeholder:text-white border-2 placeholder:font-semibold"/>
+					<span className="font-bold text-3xl ">What&apos;s your name?</span>
+					<Input
+						type="username"
+						id="username"
+						placeholder="Username"
+						className="placeholder:text-black placeholder:text-xl text-black text-xl p-6 bg-white border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,0.5)]"
+					/>
 				</div>
 			</div>
-
 		</div>
 	);
 }
